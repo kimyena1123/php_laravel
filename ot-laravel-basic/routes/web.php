5f809ca079fb21514c6b9f8d136e6982b817b540
kimyena1123 <kyq104@gmai.com>
2024-06-26 18:04:27 +0900

    feat: 글 저장하는 방법1(feat.web.php)

--- ot-laravel-basic/routes/web.php
+++ ot-laravel-basic/routes/web.php
@@ -25,7 +25,7 @@ Route::get('/articles/create', function(){
 });
 Route::post('/articles', function(Request $request){
     //비어있지 않고, 문자열이고, 255자를 넘으면 안된다.
-    $request->validate([
+    $input = $request->validate([
         'body'=>[
             'required',
             'string',
@@ -33,6 +33,27 @@ Route::post('/articles', function(Request $request){
         ],
     ]);
 
+    $host = config('database.connections.mysql.host');
+    $dbname = config('database.connections.mysql.database');
+    $username = config('database.connections.mysql.username');
+    $password = config('database.connections.mysql.password');
+
+
+    //pdo 객체
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+    //쿼리 준비
+    $stmt = $conn->prepare("INSERT INTO articles (body, user_id) VALUES (:body, :user_id)");
+
+    //쿼리 값을 설정
+    $body = $request->input('body'); // body값 가져와서 변수에 담기
+    //dd($request->collect());
+
+    $stmt->bindValue(':body', $input['body']);
+    $stmt->bindValue(':user_id', Auth::id());
+
+    //실행
+    $stmt->execute();
 
     return 'hello';
 });
