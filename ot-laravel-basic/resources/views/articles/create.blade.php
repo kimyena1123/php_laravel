f81d0ef741ebd283b91b2b0781cec60e609187a3
kimyena1123 <kyq104@gmai.com>
2024-06-26 16:22:09 +0900

    feat: lang library 받기

--- ot-laravel-basic/resources/views/articles/create.blade.php
+++ ot-laravel-basic/resources/views/articles/create.blade.php
@@ -17,7 +17,7 @@
             <input type="text" name="body" class="block w-full mb-2 rounded" value="{{old('body')}}">
 
             @error('body')
-            <p class="text-xs text-red-500 mb-3">{{ $message }}</p>
+                <p class="text-xs text-red-500 mb-3">{{ $message }}</p>
             @enderror
 
             <button class="py-1 px-3 bg-black text-white rounded text-xs">
